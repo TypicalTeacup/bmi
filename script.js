ea776051d065194d066919083c78eaec4dbafc8e
TypicalTeacup <pskirel@wp.pl>
2022-11-05 13:05:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let interpretOut = document.getElementById('resultInterpreted');
 let resultsDiv = document.getElementById('results');
 let imageOut = document.getElementById('img');
 
-function parseBMI(bmi){
-    return bmi < 16.5  ? "wygłodzenie" : 
-          (bmi < 17.0) ? "wychudzenie" :
-          (bmi < 18.5) ? "niedowaga"   :
-          (bmi < 25.0) ? "prawidłowa"  :
-          (bmi < 30.0) ? "nadwaga"     :
-                         "otyłość"     ;
+function parseBMI(bmi) {
+    return bmi < 16.5 ? "wygłodzenie" :
+        (bmi < 17.0) ? "wychudzenie" :
+        (bmi < 18.5) ? "niedowaga" :
+        (bmi < 25.0) ? "prawidłowa" :
+        (bmi < 30.0) ? "nadwaga" :
+        "otyłość";
 }
 
 let parsedToSrc = {
@@ -22,30 +22,48 @@ let parsedToSrc = {
     "otyłość"    : "otylosc.png"
 }
 
-function calculateBMI(weight, height){
-    return weight/(height**2);
+function calculateBMI(weight, height) {
+    return weight / (height ** 2);
 }
 
-function showResults(bmi){
+function showResults(bmi) {
     let parsed = parseBMI(bmi);
 
     numOut.innerHTML = `Twoje BMI to: ${bmi.toFixed(1)}`;
     interpretOut.innerHTML = `Interpretacja: ${parsed}`;
 
-    numOut.style.animation = "fadeIn 1s ease 1s 1 normal forwards";
-    interpretOut.style.animation = "fadeIn 1s ease 1s 1 normal forwards";
+    numOut.style.animation = null;
+    interpretOut.style.animation = null;
+    setTimeout(() => {
+        numOut.style.animation = "fadeIn 1s ease 0s 1 normal forwards";
+        interpretOut.style.animation = "fadeIn 1s ease 0s 1 normal forwards";
+    }, 1000);
     form.style.top = "25%";
 
+    imageOut.style.animation = null;
+    imageOut.style.bottom = '-100vh';
     imageOut.src = parsedToSrc[parsed];
-    imageOut.style.animation = "imageShow 1s cubic-bezier(.68,-0.55,.35,1.35) 0s 1 normal forwards";
+    setTimeout(function () {
+        imageOut.style.animation = "imageShow 1.5s cubic-bezier(.68,-0.55,.35,1.35) 0s 1 normal forwards";
+    }, 300);
 }
 
-function buttonOnClick(){
-    console.log("on click")
+function buttonOnClick() {
     let weight = form.weight.value;
-    let height = form.height.value/100;
+    let height = form.height.value / 100;
 
     let bmi = calculateBMI(weight, height);
 
     showResults(bmi);
+}
+
+let headerTexts = [
+    "Bardzo Mocne Igły",
+    "Bydgoszcz: Miasto Idiotów",
+    "Bardzo Mięsny Iławianin",
+    "Babcia Ma Imprezę"
+]
+
+function changeHeader() {
+    document.getElementById('header').innerHTML = headerTexts[Math.floor(Math.random() * headerTexts.length)];
 }
